5dd99cea8b55e15ca467de15f91986821cd4635e
DerEpic55 <dima20021234@gmail.com>
2026-04-19 16:32:07 +0600

    collection01

--- src/collection/collection01.ts
+++ src/collection/collection01.ts
@@ -1,9 +1,12 @@
 /* 
-	Создайте генераторную функцию numberGenerator, которая принимает число max и возвращает генератор, yielding числа от 1 до max включительно.
+	Создайте генераторную функцию numberGenerator, которая принимает число max и возвращает генератор, 
+	yielding числа от 1 до max включительно.
 */
 
-export function* numberGenerator(max: number) {
-
+export function* numberGenerator(max: number) : Generator<number> {
+	for(let i = 1; i <= max; i++){
+		yield i;
+	}
 }
 
 
