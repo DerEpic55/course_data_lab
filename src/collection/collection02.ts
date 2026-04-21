4dae39d7893b96b81950661fabfe6500783271ae
DerEpic55 <dima20021234@gmail.com>
2026-04-21 11:19:11 +0600

    collection02

--- src/collection/collection02.ts
+++ src/collection/collection02.ts
@@ -1,5 +1,6 @@
 /* 
-	Реализуйте итератор для объекта obj, чтобы при использовании в цикле for...of он возвращал значения свойств объекта, а не ключи.
+	Реализуйте итератор для объекта obj, чтобы при использовании в цикле for...of 
+  он возвращал значения свойств объекта, а не ключи.
 */
 
 export const obj = {
@@ -7,7 +8,22 @@ export const obj = {
   b: 2,
   c: 3,
   [Symbol.iterator]() {
-
+    const values = Object.values(this);
+    let index = 0;
+    return {
+      next: () => {
+        if (index < values.length) {
+          return {
+            value: values[index++],
+            done: false
+          };
+        } else {
+          return {
+            value: undefined,
+            done: true
+          };
+        }
+      }
+    };
   }
 };
-
