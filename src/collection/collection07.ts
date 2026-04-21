5d59b781eec1423cb98dcf171697a08a86159cf0
DerEpic55 <dima20021234@gmail.com>
2026-04-21 11:39:41 +0600

    collection07

--- src/collection/collection07.ts
+++ src/collection/collection07.ts
@@ -5,5 +5,10 @@
 export function countFrequency(arr: string[]): Map<string, number> {
   const map = new Map();
 
+  for (const str of arr) {
+    const count = map.get(str) || 0;
+    map.set(str, count + 1);
+  }
+
   return map;
 }
