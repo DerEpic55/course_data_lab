e324c8391222fdd8ff25e15aea965bdd54249703
DerEpic55 <dima20021234@gmail.com>
2026-04-23 16:00:08 +0600

    collection11

--- src/collection/collection11.ts
+++ src/collection/collection11.ts
@@ -1,7 +1,17 @@
 /* 
-	Создайте функцию groupBy, которая принимает массив объектов и ключ, возвращает Map, где ключи - значения этого свойства, а значения - массивы объектов с таким значением свойства.
+	Создайте функцию groupBy, которая принимает массив объектов и ключ, 
+	возвращает Map, где ключи - значения этого свойства, а значения - массивы объектов с 
+	таким значением свойства.
 */
 
 export function groupBy<T extends Record<string, any>>(arr: T[], key: keyof T): Map<T[keyof T], T[]> {
-
+	return arr.reduce((acc, item) => {
+		const groupKey = item[key];
+		const group = acc.get(groupKey) ?? [];
+		group.push(item);
+		acc.set(groupKey, group);
+		return acc;
+	}, 
+	new Map<T[keyof T], T[]>
+	);
 }
