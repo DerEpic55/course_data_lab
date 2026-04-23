ec1204bc307ef156da372e2623b55a9aff62d5c0
DerEpic55 <dima20021234@gmail.com>
2026-04-23 17:19:23 +0600

    collection21

--- src/collection/collection21.ts
+++ src/collection/collection21.ts
@@ -1,9 +1,16 @@
 /* 	
-	Создайте функцию getDiscountedInStockTotal, которая возвращает общую стоимость всех товаров в корзине, которые есть в наличии и имеют скидку.
+	Создайте функцию getDiscountedInStockTotal, которая возвращает общую стоимость 
+	всех товаров в корзине, которые есть в наличии и имеют скидку.
 */
 
 export type Product = { price: number; discount: boolean; inStock: boolean };
 
 export function getDiscountedInStockTotal(products: Product[]): number {
+	return products.
+	filter(product => product.discount && product.inStock).
+	reduce((acc, product) => {
+		acc += product.price
+		return acc;
+	}, 0);
 
 }
