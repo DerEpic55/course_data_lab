0c0b21e26da01e92e0e89905b9107a32a43d821e
DerEpic55 <dima20021234@gmail.com>
2026-04-23 16:37:45 +0600

    collection16

--- src/collection/collection16.ts
+++ src/collection/collection16.ts
@@ -1,9 +1,14 @@
 /* 
-	Создайте функцию squaresWithIndex, которая принимает массив чисел и возвращает массив объектов с исходным числом, его квадратом и индексом.
+	Создайте функцию squaresWithIndex, которая принимает массив чисел и возвращает массив объектов с 
+	исходным числом, его квадратом и индексом.
 */
 
 export type Result = {num: number, square: number, index: number};
 
 export function squaresWithIndex(numbers: number[]): Result[] {
-
+	return numbers.map((num, index) : Result => ({
+		num: num,
+		square: num*num,
+		index: index
+	}));
 }
