37f416a747000fdb3ddc24c6c4ccc5ddf60b091a
DerEpic55 <dima20021234@gmail.com>
2026-04-23 14:28:19 +0600

    collection09

--- src/collection/collection09.ts
+++ src/collection/collection09.ts
@@ -1,9 +1,16 @@
 /* 
-	Напишите функцию findIntersection, которая принимает два массива и возвращает массив их общих элементов, используя Set.
+	Напишите функцию findIntersection, которая принимает два массива и 
+  возвращает массив их общих элементов, используя Set.
 */
 
 export function findIntersection(arr1: number[], arr2: number[]): number[] {
   const intersection = new Set<number>();
-
+  for(const value1 of arr1){
+    for(const value2 of arr2){
+      if(value1 === value2){
+         intersection.add(value1);
+      }
+    }
+  }
   return [...intersection];
 }
