3c01d4a37028fe8a0423a9a174a0fdb1fd552a2b
DerEpic55 <dima20021234@gmail.com>
2026-04-23 16:45:53 +0600

    collection18

--- src/collection/collection18.ts
+++ src/collection/collection18.ts
@@ -1,9 +1,10 @@
 /* 	
-	Напишите функцию getAffordableInStockProducts, которая фильтрует продукты по цене (дешевле 1000) и наличию на складе, затем возвращает их названия.
+	Напишите функцию getAffordableInStockProducts, которая фильтрует продукты по цене (дешевле 1000) 
+	и наличию на складе, затем возвращает их названия.
 */
 
 type Product = {name: string, price: number, inStock: boolean};
 
 export function getAffordableInStockProducts(products: Product[]): string[] {
-
+	return products.filter(product => product.price < 1000 && product.inStock).map(product => product.name);
 }
