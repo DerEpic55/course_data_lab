1837536e1bd6b5d1e43715adb66f250067c8e121
DerEpic55 <dima20021234@gmail.com>
2026-04-23 16:44:00 +0600

    collection17

--- src/collection/collection17.ts
+++ src/collection/collection17.ts
@@ -1,10 +1,11 @@
 /* 
-	Создайте функцию getVerifiedEmails, которая принимает массив пользователей и возвращает массив email-адресов только тех пользователей, у которых email подтвержден.
+	Создайте функцию getVerifiedEmails, которая принимает массив пользователей и возвращает 
+	массив email-адресов только тех пользователей, у которых email подтвержден.
 */
 
 export type User = {email: string, verified: boolean};
 
 export function getVerifiedEmails(users: User[]): string[] {
-	
+	return users.filter(user => user.verified).map(user => user.email);
 }
 
