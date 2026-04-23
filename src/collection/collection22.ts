913759a443d5c6eebe9824d38810d226d332bfec
DerEpic55 <dima20021234@gmail.com>
2026-04-23 17:25:16 +0600

    collection22

--- src/collection/collection22.ts
+++ src/collection/collection22.ts
@@ -1,9 +1,17 @@
 /* 
-	Создайте функцию sumProperty, которая принимает массив объектов с числовыми свойствами и возвращает сумму всех значений указанного свойства.
+	Создайте функцию sumProperty, которая принимает массив объектов 
+	с числовыми свойствами и возвращает сумму всех значений указанного свойства.
 */
 
 export function sumProperty<T>(objects: T[], property: keyof T): number {
-	
+	return objects.reduce(
+		(acc, object) =>{
+			acc += object[property] as number;
+
+			return acc;
+		},
+		0
+	);
 }
 
 
