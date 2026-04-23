d52c0545b118b677c1abe7c7d8d9c27d3844ab8a
DerEpic55 <dima20021234@gmail.com>
2026-04-23 17:09:46 +0600

    collection20

--- src/collection/collection20.ts
+++ src/collection/collection20.ts
@@ -1,11 +1,26 @@
 /* 	
-	Напишите функцию groupUnfinishedHighPriorityTasks, которая группирует задачи по категориям, фильтруя только незавершенные высокоприоритетные задачи.
+	Напишите функцию groupUnfinishedHighPriorityTasks, 
+	которая группирует задачи по категориям, 
+	фильтруя только незавершенные высокоприоритетные задачи.
 */
 
 export type Task = { category: string; priority: string; completed: boolean };
 
 export function groupUnfinishedHighPriorityTasks(tasks: Task[]): Map<string, Task[]> {
+	return tasks.reduce(
+		(acc, task) => {
+			if(!task.completed && task.priority === 'high'){
+				const category = task.category;
+				const group = acc.get(category) ?? [];
+				
+				group.push(task);
+				acc.set(category, group);
+			}
 
+			return acc;
+		},
+		new Map<string, Task[]>
+	);
 }
 
 
