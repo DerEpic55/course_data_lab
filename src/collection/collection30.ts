b8f3990227d3b860aae9a6beadab71d6e23ba9ab
DerEpic55 <dima20021234@gmail.com>
2026-04-24 18:44:13 +0600

    collection30

--- src/collection/collection30.ts
+++ src/collection/collection30.ts
@@ -1,11 +1,12 @@
 /* 
-	Создайте функцию getVerifiedAdultEmails, которая преобразует массив пользователей в массив email-адресов, но только для верифицированных пользователей старше 18 лет.
+	Создайте функцию getVerifiedAdultEmails, которая преобразует массив пользователей в 
+	массив email-адресов, но только для верифицированных пользователей старше 18 лет.
 */
 
 type User = {email: string, verified: boolean, age: number}
 
 export function getVerifiedAdultEmails(users: User[]): string[] {
-	
+	return users.filter(user => user.verified === true && user.age > 18).map(user => user.email);
 }
 
 
