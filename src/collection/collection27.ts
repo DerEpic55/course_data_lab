f92552de2c9439b2939d521ec4a31382a41e3686
DerEpic55 <dima20021234@gmail.com>
2026-04-24 18:16:22 +0600

    collection27

--- src/collection/collection27.ts
+++ src/collection/collection27.ts
@@ -1,8 +1,21 @@
-/* 	Напишите функцию findMostPopularTag, которая находит самый часто встречающийся тег в массиве постов и количество его повторений. Функция не должна использовать циклы.
+/* 	Напишите функцию findMostPopularTag, которая находит самый часто встречающийся тег в массиве постов 
+и количество его повторений. Функция не должна использовать циклы.
 */
 
 type Post = { tags: string[] };
 
 export function findMostPopularTag(posts: Post[]): Record<string, number> {
-
+    return posts.reduce(
+        (acc, post) => {
+            post.tags.reduce(
+                (acc, tag) =>{
+                    acc[tag] = (acc[tag] || 0) + 1;
+                    return acc;
+                },
+                acc
+            ); 
+            return acc;
+        },
+        {} as Record<string, number>
+    );
 }
