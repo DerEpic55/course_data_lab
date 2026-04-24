5f08e95ddd31c7386e947406d3dc677459c6d84f
DerEpic55 <dima20021234@gmail.com>
2026-04-24 19:56:19 +0600

    serial02

--- src/serial/serial02.ts
+++ src/serial/serial02.ts
@@ -1,11 +1,13 @@
 /* 
 	Модифицируйте класс Team, чтобы массив members корректно восстанавливался как массив объектов User.
 */
-
+import 'reflect-metadata';
+import { Type } from "class-transformer"
 import { User } from "./user";
 
 export class Team {
   name: string;
+  @Type(() => User)
   members: User[]; // Требует декоратора
   
   constructor(name: string, members: User[]) {
@@ -13,5 +15,7 @@ export class Team {
 	this.members = members;
   }
   
-  listMembers() { return this.members.map(m => m.getFullName()).join(', '); }
+  listMembers() { 
+    return this.members.map(m => m.getFullName()).join(', '); 
+  }
 }
