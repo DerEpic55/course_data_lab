13aba163d2a6ad5396d68a8b44189aa31877df8f
DerEpic55 <dima20021234@gmail.com>
2026-04-24 18:30:52 +0600

    collection28

--- src/collection/collection28.ts
+++ src/collection/collection28.ts
@@ -1,9 +1,13 @@
 /* 
-	Создайте функцию getTopRatedInCategory, которая возвращает 3 товара с наивысшим рейтингом в указанной категории. Функция не должна использовать циклы.
+	Создайте функцию getTopRatedInCategory, которая возвращает 3 товара с наивысшим рейтингом в указанной категории. 
+	Функция не должна использовать циклы.
 */
 
 type Product = {category: string, rating: number, name: string}
 
 export function getTopRatedInCategory(products: Product[], category: string): Product[] {
-
+	return products
+		.filter(product => product.category === category)
+		.sort((productA, productB) => productB.rating - productA.rating)
+		.slice(0, 3);
 }
