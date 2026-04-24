7ce85d182ac93c58d24e32cb93f3526e343b2037
DerEpic55 <dima20021234@gmail.com>
2026-04-24 18:42:09 +0600

    collection29

--- src/collection/collection29.ts
+++ src/collection/collection29.ts
@@ -1,9 +1,18 @@
 /* 
-	Напишите функцию calculateTotalProgress, которая вычисляет общий прогресс пользователей, учитывая только активных и с прогрессом > 50%.
+	Напишите функцию calculateTotalProgress, которая вычисляет общий прогресс пользователей, 
+	учитывая только активных и с прогрессом > 50%.
 */
 
 type User = {active: boolean, progress: number}
 
 export function calculateTotalProgress(users: User[]): number {
-
+	const activeUsers : User[] = users.filter(user => user.active === true && user.progress > 50);
+	return activeUsers
+		.reduce(
+			(acc, user) => {
+				acc += user.progress;
+				return acc 
+			},
+			0
+		) / activeUsers.length;
 }
