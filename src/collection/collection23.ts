662390261317eb887bd5843227063c948afb05ee
DerEpic55 <dima20021234@gmail.com>
2026-04-24 17:07:42 +0600

    collection23

--- src/collection/collection23.ts
+++ src/collection/collection23.ts
@@ -1,9 +1,19 @@
 /* 
-	Напишите функцию groupByCategory, которая принимает массив объектов и имя свойства для группировки, возвращает объект, где ключи - значения свойства, а значения - массивы объектов.
+	Напишите функцию groupByCategory, которая принимает массив объектов и имя свойства для группировки, 
+	возвращает объект, где ключи - значения свойства, а значения - массивы объектов.
 */
 
 export function groupByCategory<T extends Record<string, any>>(arr: T[], key: keyof T): Record<T[keyof T], T[]> {
-
+	return arr.reduce((acc, item) => {
+    	const groupKey = item[key];
+    	if (!acc[groupKey]) {
+    	  acc[groupKey] = [];
+    	}
+    	acc[groupKey].push(item);
+    	return acc;
+		},
+		{} as Record<T[keyof T], T[]>
+	);
 }
 
 
