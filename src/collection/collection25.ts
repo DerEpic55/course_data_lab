16fa5d9b25575dc50e64bbec727899d94ad9df39
DerEpic55 <dima20021234@gmail.com>
2026-04-24 17:45:33 +0600

    collection25

--- src/collection/collection25.ts
+++ src/collection/collection25.ts
@@ -1,10 +1,24 @@
 /* 
-	Напишите функцию getOrderStats, которая принимает массив заказов и возвращает объект с общей суммой, количеством и средним чеком только завершенных заказов (status: 'completed').
+	Напишите функцию getOrderStats, которая принимает массив заказов и 
+	возвращает объект с общей суммой, количеством и средним чеком только завершенных заказов 
+	(status: 'completed').
 */
 
 type Order = {status: string, amount: number}
 type Stats = {total: number, count: number, average: number}
 
 export function getOrderStats(orders: Order[]): Stats{
-
+	return orders.filter(order => order.status === 'completed').reduce(
+		(acc, order) => {
+			acc.total += order.amount;
+			acc.count += 1;
+			acc.average = acc.total / acc.count;
+			return acc;
+		},
+		{
+			total: 0,
+			count: 0,
+			average: 0
+		} as Stats
+	);
 }
