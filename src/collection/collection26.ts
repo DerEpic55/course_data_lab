99e240e9db6d32a5e3e3770b91c36320171e125d
DerEpic55 <dima20021234@gmail.com>
2026-04-24 17:53:38 +0600

    collection26

--- src/collection/collection26.ts
+++ src/collection/collection26.ts
@@ -1,10 +1,29 @@
+
 /* 
-	Создайте функцию groupProductsByPrice, которая группирует товары в категории: "дешевые" (<1000), "средние" (1000-5000), "дорогие" (>5000). В функции не должно быть циклов.
+	Создайте функцию groupProductsByPrice, которая группирует товары в категории: "дешевые" (<1000), 
+	"средние" (1000-5000), "дорогие" (>5000). 
+	В функции не должно быть циклов.
 */
 
 type Product = {name: string, price: number};
 type GroupedProducts = {cheap: Product[], medium: Product[], expensive: Product[]};
 
 export function groupProductsByPrice(products: Product[]): GroupedProducts {
-
+	return products.reduce(
+		(acc, product) => {
+			if(product.price < 1000){
+				acc.cheap.push(product);
+			}else if(product.price >= 1000 && product.price <= 5000){
+				acc.medium.push(product);
+			}else{
+				acc.expensive.push(product);
+			}
+			return acc;
+		},
+		{
+			cheap : [],
+			medium: [],
+			expensive: []
+		} as GroupedProducts
+	);
 }
