13307384156c5aa3f5274713afeaedad2100b2b2
DerEpic55 <dima20021234@gmail.com>
2026-04-24 17:35:39 +0600

    collection24

--- src/collection/collection24.ts
+++ src/collection/collection24.ts
@@ -1,12 +1,26 @@
 /* 
-	Создайте функцию findMaxWithCondition, которая находит объект с максимальным значением свойства, но только среди элементов, удовлетворяющих условию фильтра, в противном случае возвращает null.
+	Создайте функцию findMaxWithCondition, которая находит объект с максимальным значением свойства, 
+	но только среди элементов, удовлетворяющих условию фильтра, в противном случае возвращает null.
 */
 
 export function findMaxWithCondition<T>(
 	array: T[], 
 	propertyName: keyof T,
 	condition: (item: T) => boolean): T | null {
+	return array.reduce(
+		(acc : T | null, currentItem : T) => {
+			if(!condition(currentItem)){
+				return acc;
+			}
 
+			if(acc === null || currentItem[propertyName] > acc![propertyName]){
+				return currentItem;
+			}
+
+			return acc;
+		},
+		null
+	);
 }
 
 
