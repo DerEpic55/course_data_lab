00ea09f97147ab4ac890bef1dbb8cc55d9e9e699
DerEpic55 <dima20021234@gmail.com>
2026-04-27 19:40:09 +0600

    serial26

--- src/serial/serial26.ts
+++ src/serial/serial26.ts
@@ -3,15 +3,21 @@
 	Пример XML в файле saxXML.ts
 */
 
-import { SAXParser } from "sax";
+import * as sax from "sax";
 
 // Исходный код
 export function collectBookIds(xml: string): string[] {
-  const parser = new SAXParser(true);
+  const parser = new sax.SAXParser(true);
   const ids: string[] = [];
-  
   // TODO: При открытии тега 'book' добавляйте значение атрибута 'id' в массив ids
-
+   parser.onopentag = (node) => {
+    if (node.name === 'book') {
+      const bookId = node.attributes['id'];
+      if (bookId) {
+        ids.push(String(bookId));
+      }
+    }
+  };
   
   parser.write(xml).close();
   return ids;
