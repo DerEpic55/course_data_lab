60492160a8c3504c20fb6e576498d8222b3d9210
DerEpic55 <dima20021234@gmail.com>
2026-04-27 20:29:05 +0600

    serial29

--- src/serial/serial29.ts
+++ src/serial/serial29.ts
@@ -3,18 +3,39 @@
 	Пример XML в файле saxXML.ts
 */
 
-import { SAXParser, type QualifiedAttribute } from "sax";
+import * as sax from "sax";
 
 
 export function findBookById(xml: string, targetId: string): string | null {
-  const parser = new SAXParser(true);
-  let currentId:string | QualifiedAttribute = '';
+  const parser = new sax.SAXParser(true);
+  let currentId:string | sax.QualifiedAttribute = '';
   let currentTitle = '';
   let foundBook = false;
   let isTitleTag = false;
   
 // TODO: Реализуйте логику поиска книги по ID и извлечения её названия
-  
+  parser.onopentag = (node: sax.QualifiedTag) => {
+    if(node.name === 'book'){
+      currentId = String(node.attributes['id']);
+    }
+    if(node.name === 'title'){
+      isTitleTag = true;
+    }
+  };
+
+  parser.ontext = (text : string) => {
+    if(isTitleTag === true && currentId === targetId){
+      foundBook = true;
+      currentTitle = text.trim();
+    }
+  };
+
+  parser.onclosetag = (tagName : string) => {
+      if(tagName === 'title'){
+        isTitleTag = false;
+      }
+  }
+
   parser.write(xml).close();
   return foundBook ? currentTitle.trim() : null;
 }
