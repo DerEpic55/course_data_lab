98b466563c5d0c33014135a0fe4a5362e3d937e4
DerEpic55 <dima20021234@gmail.com>
2026-04-27 19:54:00 +0600

    serial27

--- src/serial/serial27.ts
+++ src/serial/serial27.ts
@@ -3,10 +3,10 @@
 	Пример XML в файле saxXML.ts
 */
 
-import { SAXParser } from "sax";
+import * as sax from "sax";
 
 export function extractTitles(xml: string): string[] {
-  const parser = new SAXParser(true);
+  const parser = new sax.SAXParser(true);
   const titles: string[] = [];
   let currentTitle = "";
   let isTitleTag = false;
@@ -15,7 +15,22 @@ export function extractTitles(xml: string): string[] {
   // - открытия тега title (установите isTitleTag = true)
   // - текстового содержимого (если isTitleTag, добавьте к currentTitle)
   // - закрытия тега title (добавьте currentTitle в titles и сбросьте значения)
+  parser.onopentag = (node: sax.QualifiedTag) => {
+    if(node.name === 'title')
+      isTitleTag = true;
+  };
 
+  parser.ontext = (text: string) => {
+    if(isTitleTag) currentTitle = text.trim();
+  }
+
+  parser.onclosetag = (tagName: string) => {
+    if(tagName === 'title'){
+        titles.push(currentTitle);
+        currentTitle = "";
+        isTitleTag = false;
+    }
+  }
 
   parser.write(xml).close();
   return titles;
