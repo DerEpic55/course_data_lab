11917dd54ba97244f5bde17bd0de7d67e44aa3d2
DerEpic55 <dima20021234@gmail.com>
2026-04-27 20:00:07 +0600

    serial28

--- src/serial/serial28.ts
+++ src/serial/serial28.ts
@@ -3,15 +3,21 @@
 	Пример XML в файле saxXML.ts
 */
 
-import { SAXParser } from "sax";
+import * as sax from "sax";
 
 // Исходный код
 export function validateBookStructure(xml: string): boolean {
-  const parser = new SAXParser(true);
+  const parser = new sax.SAXParser(true);
   let isValid = true;
 
   // TODO: Если встречается тег 'book' без атрибута 'id', установите isValid = false
-
+  parser.onopentag = (node : sax.QualifiedTag) => {
+      if(node.name === 'book'){
+        if(!node.attributes['id']){
+          isValid = false;
+        }
+      }
+  };
   parser.write(xml).close();
   return isValid;
 }
