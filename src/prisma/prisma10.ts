b24aeb8f614ba108c303b168edce89b984949605
DerEpic55 <dima20021234@gmail.com>
2026-05-11 21:05:05 +0600

    prisma10

--- src/prisma/prisma10.ts
+++ src/prisma/prisma10.ts
@@ -3,4 +3,16 @@ import { prisma } from './prisma_init'
 export async function get_course_average_grade(courseTitle: string): Promise<number | null> {
     // TODO: Найти среднюю оценку по указанному курсу
     // Вернуть среднее значение или null если курс не найден или нет оценок
+    const result = await prisma.grade.aggregate({
+        where: {
+            course:{
+                title: courseTitle
+            }
+        },
+        _avg:{
+            grade: true
+        }
+    });
+
+    return result._avg.grade;
 }
