f8adc4fd868c8b2419d73dcb66f4afc1a1934edc
DerEpic55 <dima20021234@gmail.com>
2026-05-12 20:04:35 +0600

    prisma19

--- src/prisma/prisma19.ts
+++ src/prisma/prisma19.ts
@@ -4,4 +4,25 @@ export async function find_most_popular_course() {
     // TODO: Найти курс с максимальным количеством уникальных студентов
     // Использовать агрегацию средствами Prisma (groupBy)
     // Вернуть объект курса с дополнительным полем studentCount
+    const groupResult = await prisma.grade.groupBy({
+        by: ['courseId'],
+        _count: {
+            studentId: true,
+        },
+        orderBy: {
+            _count: {
+                studentId: 'desc',
+            },
+        },
+        take: 1,
+    });
+
+    const course = await prisma.course.findUnique({
+        where: { id: groupResult[0].courseId }
+    });
+
+    return {
+        ...course,
+        studentCount: groupResult[0]._count.studentId
+    };
 }
