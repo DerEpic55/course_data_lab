01268bd29d6969e739cb44d4a464e7cf54a64550
DerEpic55 <dima20021234@gmail.com>
2026-05-12 19:36:33 +0600

    prisma15

--- src/prisma/prisma15.ts
+++ src/prisma/prisma15.ts
@@ -4,4 +4,16 @@ export async function update_low_grades() {
     // TODO: Обновить все оценки ниже 3 на значение 3
     // Использовать один запрос updateMany средствами Prisma
     // Вернуть количество обновленных записей
+    const result = await prisma.grade.updateMany({
+        data:{
+            grade: 3
+        },
+        where:{
+            grade: {
+                lt: 3
+            }
+        }
+    });
+
+    return result.count;
 }
