1b2e3ab7861154008f193b4843671990c6d0b661
DerEpic55 <dima20021234@gmail.com>
2026-05-12 19:24:11 +0600

    prisma13

--- src/prisma/prisma13.ts
+++ src/prisma/prisma13.ts
@@ -6,4 +6,26 @@ export async function find_top_courses(limit: number) {
     // Вернуть массив с id курса, названием и средней оценкой
     // Отсортировать по убыванию средней оценки
     // Использовать вычисление средней оценки в TypeScript коде (не через агрегацию Prisma)
+        const courses = await prisma.course.findMany({
+        include: {
+            grades: true
+        }
+    });
+
+    const courseStats = courses
+        .filter(course => course.grades.length > 0)
+        .map(course => {
+            const sum = course.grades.reduce((acc, curr) => acc + curr.grade, 0);
+            const avg = sum / course.grades.length;
+
+            return {
+                id: course.id,
+                title: course.title, 
+                averageGrade: avg
+            };
+        });
+
+    courseStats.sort((a, b) => b.averageGrade - a.averageGrade);
+
+    return courseStats.slice(0, limit);
 }
