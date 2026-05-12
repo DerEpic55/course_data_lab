4f5650f8051620e62add65b30960325587f2c8ec
DerEpic55 <dima20021234@gmail.com>
2026-05-12 20:52:32 +0600

    prisma25

--- src/prisma/prisma25.ts
+++ src/prisma/prisma25.ts
@@ -4,4 +4,16 @@ export async function update_courses_without_grades_description() {
     // TODO: Обновить описание курсов без оценок на "Нет оценок"
     // Использовать один запрос updateMany средствами Prisma
     // Вернуть количество обновленных курсов
+    const result = await prisma.course.updateMany({
+        data:{
+            description: 'Нет оценок'
+        },
+        where: {
+            grades:{
+                none: {}
+            }
+        }
+    });
+
+    return result.count;
 }
