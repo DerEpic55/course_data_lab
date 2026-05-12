1c7b3d371344fee88d72ee8a2088bcfb17d80dec
DerEpic55 <dima20021234@gmail.com>
2026-05-12 21:16:18 +0600

    prisma27

--- src/prisma/prisma27.ts
+++ src/prisma/prisma27.ts
@@ -3,4 +3,19 @@ import { prisma } from './prisma_init'
 export async function find_courses_with_excellent_students() {
     // TODO: Найти курсы с количеством студентов, имеющих оценку 5 по этому курсу
     // Вернуть массив курсов с дополнительным полем excellentCount
+    const courses = await prisma.course.findMany({
+        include: {
+            grades: {
+                where: {
+                    grade: 5
+                }
+            }
+        }
+    });
+
+    return courses.map(course => ({
+        id: course.id,
+        title: course.title, 
+        excellentCount: course.grades.length
+    }));
 }
