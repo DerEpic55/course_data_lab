4c9656da50fbc534fa788fe5739b223c55feffeb
DerEpic55 <dima20021234@gmail.com>
2026-05-12 20:58:43 +0600

    prisma26

--- src/prisma/prisma26.ts
+++ src/prisma/prisma26.ts
@@ -3,4 +3,24 @@ import { prisma } from './prisma_init'
 export async function find_oldest_and_newest_students() {
     // TODO: Найти самого старого и самого нового студента по дате создания
     // Вернуть объект { oldest: студент, newest: студент } с информацией о person
+    const oldest = await prisma.student.findFirst({
+        orderBy: [
+            { createdAt: 'asc' },
+            { id: 'asc' }
+        ],
+        include: { person: true }
+    });
+
+    const newest = await prisma.student.findFirst({
+        orderBy: [
+            { createdAt: 'desc' },
+            { id: 'desc' }
+        ],
+        include: { person: true }
+    });
+
+    return {
+        oldest,
+        newest
+    };
 }
