4d02ae68a0d0eb550c8ee46e498428831b569645
DerEpic55 <dima20021234@gmail.com>
2026-05-12 18:48:12 +0600

    prisma11

--- src/prisma/prisma11.ts
+++ src/prisma/prisma11.ts
@@ -5,4 +5,38 @@ export async function update_students_email_domain(oldDomain: string, newDomain:
     // Заменить домен на newDomain
     // Вернуть количество обновленных записей
 	// Допускается обновление email с помощью простого цикла с запросами в теле цикла
+        const students = await prisma.student.findMany({
+        where: {
+            person: {
+                email: {
+                    endsWith: oldDomain
+                }
+            }
+        },
+        select: {
+            personId: true,
+            person: {
+                select: {
+                    email: true
+                }
+            }
+        }
+    });
+
+    let updatedCount = 0;
+
+    for (const student of students) {
+        if (student.person) {
+            const newEmail = student.person.email.replace(oldDomain, newDomain);
+            
+            await prisma.person.update({
+                where: { id: student.personId },
+                data: { email: newEmail }
+            });
+
+            updatedCount++;
+        }
+    }
+
+    return updatedCount;
 }
